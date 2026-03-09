2d3007cf84f80ab8ba6594073b25310d439d51aa
KayasSecret <kayasmishra29s@gmail.com>
2026-03-09 20:45:01 +0530

    #Update

--- twenty.c
+++ twenty.c
@@ -5,6 +5,11 @@
 int main() {
     int n, first = 0, second = 1, next, i;
 
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
     for(i = 1; i <= n; i++) {
         if(i == 1) {
             printf("%d, ", first);
