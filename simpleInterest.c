7bacee9060869912b8bed2fa603dd09f47978260
KayasSecret <kayasmishra29s@gmail.com>
2026-03-09 20:44:32 +0530

    #Update

--- simpleInterest.c
+++ simpleInterest.c
@@ -6,6 +6,15 @@ int main() {
     int pri_amount , time, rate;
     float Si_In;
 
+    printf("Enter the Principal Amount - ");
+    scanf("%d", &pri_amount);
+
+    printf("Enter the Time - ");
+    scanf("%d", &time);
+
+    printf("Enter the Rate - ");
+    scanf("%d", &rate);
+
     Si_In = (pri_amount * time * rate) / 100.0;
 
     printf("Your answer is - %.2f", Si_In);
