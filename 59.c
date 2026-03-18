94e1e7d82435ae54806e5b393c145a2fd3ff1473
KayasSecret <kayasmishra29s@gmail.com>
2026-03-18 21:34:21 +0530

    #Update

---
+++ 59.c
@@ -0,0 +1,72 @@
+// Insert at Position
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertAtEnd(struct Node* head, int value) {
+    struct Node* newNode = createNode(value);
+
+    if (head == NULL) return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+struct Node* insertAtPosition(struct Node* head, int value, int pos) {
+    struct Node* newNode = createNode(value);
+
+    if (pos == 1) {
+        newNode->next = head;
+        return newNode;
+    }
+
+    struct Node* temp = head;
+    for (int i = 1; i < pos - 1 && temp != NULL; i++) {
+        temp = temp->next;
+    }
+
+    if (temp != NULL) {
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+
+    return head;
+}
+
+void display(struct Node* head) {
+    while (head != NULL) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    head = insertAtEnd(head, 10);
+    head = insertAtEnd(head, 20);
+    head = insertAtEnd(head, 40);
+
+    head = insertAtPosition(head, 30, 3);
+
+    display(head);
+
+    return 0;
+}
