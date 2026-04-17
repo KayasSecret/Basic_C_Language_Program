7e3c35c97cf3cd74de69ed14707220592f0dfa21
KayasSecret <kayasmishra29s@gmail.com>
2026-04-17 18:46:32 +0530

    #Update

--- 60.c
+++ 60.c
@@ -5,25 +5,3 @@ struct node {
     int data;
     struct node *left, *right;
 };
-
-struct node* create() {
-    int x;
-    struct node *newnode;
-
-    printf("Enter data (-1 for no node): ");
-    scanf("%d", &x);
-
-    if(x == -1)
-        return 0;
-
-    newnode = (struct node*)malloc(sizeof(struct node));
-    newnode->data = x;
-
-    printf("Enter left child of %d:\n", x);
-    newnode->left = create();
-
-    printf("Enter right child of %d:\n", x);
-    newnode->right = create();
-
-    return newnode;
-}
